f595174db73f31c29bad5f2d0b764f662088c7f8
Oscar Hill <OscarPi@users.noreply.github.com>
2025-04-28 15:08:45 +0000

    Update baseline code

---
+++ run_baselines.sh
@@ -0,0 +1,11 @@
+python experiments/run_baselines.py -c experiments/configs/mnist_add.yaml
+python experiments/run_baselines.py -c experiments/configs/mnist_add.yaml
+python experiments/run_baselines.py -c experiments/configs/mnist_add.yaml
+
+python experiments/run_baselines.py -c experiments/configs/shapes.yaml
+python experiments/run_baselines.py -c experiments/configs/shapes.yaml
+python experiments/run_baselines.py -c experiments/configs/shapes.yaml
+
+python experiments/run_baselines.py -c experiments/configs/cub.yaml
+python experiments/run_baselines.py -c experiments/configs/cub.yaml
+python experiments/run_baselines.py -c experiments/configs/cub.yaml
