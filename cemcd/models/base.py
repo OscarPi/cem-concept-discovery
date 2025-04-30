c69669978cd4dbae7ccc4fb20399289ecb5fe036
Oscar Hill <OscarPi@users.noreply.github.com>
2025-04-30 12:20:08 +0000

    Remove deprecated thing

--- cemcd/models/base.py
+++ cemcd/models/base.py
@@ -118,8 +118,7 @@ class BaseModel(lightning.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer,
-            verbose=True,
+            optimizer
         )
         return {
             "optimizer": optimizer,
