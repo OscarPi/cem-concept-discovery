49c091d46170789632370bd6617f338a6a2b0664
Oscar Hill <OscarPi@users.noreply.github.com>
2025-05-03 14:13:22 +0000

    Prepare AwA experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,11 +1,3 @@
-python experiments/run_experiment.py -c experiments/configs/cub.yaml
-python experiments/run_experiment.py -c experiments/configs/cub.yaml
-python experiments/run_experiment.py -c experiments/configs/cub.yaml
-
-python experiments/run_experiment.py -c experiments/configs/mnist_add.yaml
-python experiments/run_experiment.py -c experiments/configs/mnist_add.yaml
-python experiments/run_experiment.py -c experiments/configs/mnist_add.yaml
-
-python experiments/run_experiment.py -c experiments/configs/shapes.yaml
-python experiments/run_experiment.py -c experiments/configs/shapes.yaml
-python experiments/run_experiment.py -c experiments/configs/shapes.yaml
+python experiments/run_experiment.py -c experiments/configs/awa.yaml
+python experiments/run_experiment.py -c experiments/configs/awa.yaml
+python experiments/run_experiment.py -c experiments/configs/awa.yaml
